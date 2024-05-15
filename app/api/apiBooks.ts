ccf55c43fd0ff43e27f8deeb8e2d84f798d3b415
Edson Carlos <edsoncarlos@ymail.com>
2024-05-15 01:21:40 -0300

    chore: Alterando extensão do arquivo config do next

--- app/api/apiBooks.ts
+++ app/api/apiBooks.ts
@@ -1,6 +1,5 @@
 export async function getAllBooks(query: any) {
-  const API_KEY = 'guardada';
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY}`;
 
   const response = await fetch(url);
   const data = await response.json();
